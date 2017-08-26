36a42cc53b7bbb07c138962a9e7b47ae3edcd33b
Kevin Seagraves <k.s.seagraves@gmail.com>
2017-08-26 14:20:07 -0700

    more props done

--- src/components/ReceiveProcess.js
+++ src/components/ReceiveProcess.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
 
 
 class Receive_Process extends Component {
 
   render() {
     return (
-      <div className="container">
+
         <div className="row 150%">
           <div className="6u 12u$(medium)">
             <header className="major">
@@ -17,11 +16,9 @@ class Receive_Process extends Component {
             <p>When Confired, TX Address: 0x36digithash</p>
             <p>Congratulations! You're special! Welcome to the future of the internet!</p>
 
-            <Link to="/send"><input type="submit" value="Send ETH to a friend" className="special" /></Link>
-
           </div>
         </div>
-      </div>  
+
     );
   }
 }
