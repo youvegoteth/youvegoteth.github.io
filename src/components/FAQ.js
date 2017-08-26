802a26a24f017d9a17d926c755b394a76d3978c5
Paul Foley <me@paulnicholasfoley.com>
2017-08-26 15:24:15 -0700

    Add FAQ

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -10,7 +10,15 @@ class FAQ extends Component {
             <header className="major">
               <h2>FAQ</h2>
             </header>
-            <p>Here are some things you probably want to know about since you can't seem to understand how to use it.</p>
+            <h3>Can my grandmother use this?</h3>
+            <p>Yes</p>
+            <h3>Is this safe?</h3>
+            <p>Yes</p>
+            <h3>Can I send this to any email address?</h3>
+            <p>Yes</p>
+          </div>
+          <div class="6u$ 12u$(medium) important(medium)">
+            <span class="image fit"><img src="images/pic01.jpg" alt="" /></span>
           </div>
         </div>
       </div>
