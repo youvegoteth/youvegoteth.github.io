3333e52a1bffcfa57104b607b78c2704a50e6231
Paul Foley <me@paulnicholasfoley.com>
2017-08-26 19:26:54 -0700

    Update Copy

--- src/components/Education.js
+++ src/components/Education.js
@@ -12,8 +12,12 @@ class Education extends Component {
               <div className="mini-major">
                 <h2 className="h2-underline">Education</h2>
               </div>
+              <h3>Using "You've Got ETH" is simple</h3>
+              <p>To get started all you need to do is put in your friends <b>email address</b> as well as the amount of <a href="https://www.ethereum.org/">Ethereum</a> you want to send.</p>
+              <p>(Then if you're so enclined leave us a tip, that's how we survive!)</p>
+              <p>An email will be sent to the address you specified, with easy instructions for them to setup a <a href="https://metamask.io/">MetaMask</a> account and get started with Ethereum.</p>
             </div>
-            <p>Some awesome copy about how to use this awesome pla</p>
+            
           </div>
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,49 +9,48 @@ class Home extends Component {
   render() {
     return (
       <div className="App-intro-bar">
-          <Particles params={{
-         particles: {
-           number: {
-             value: 50
-           },
-           line_linked: {
-             shadow: {
-               enable: false,
-               color: "#3CA9D1",
-               blur: 1
-             }
-           }
-         }
-       }} style={{
-         position: "fixed",
-         top: 0,
-         left: 0,
-         width: "100%",
-         height: "100%"
-       }}/>
-      <div className="IntroBar-width">
-        <h1 className="IntroBar-title">Evangelize ETH</h1>
-          <h3 className="h3-thing">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Mauris massa. Vestibulum lacinia arcu eget nulla.
-          </h3>
+        <Particles params={{
+          particles: {
+            number: {
+              value: 50
+            },
+            line_linked: {
+              shadow: {
+                enable: false,
+                color: "#3CA9D1",
+                blur: 1
+              }
+            }
+          }
+        }} style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%"
+        }} />
+        <div className="IntroBar-width">
+          <h1 className="IntroBar-title">You've Got ETH</h1>
+          <h3 className="h3-thing">Evangalize Ethereum</h3>
           <div className="IntroBar-benefits-div">
             <div className="IntroBar-benefits">
               <div className="circle"/>
-              <span className="IntroBar-benefits-span">Friendly</span>
-              <p>Send to friends who know little about Eth.</p>
-            </div>
-            <div className="IntroBar-benefits">
-              <div className="circle"/>
-              <span className="IntroBar-benefits-span">Fast</span>
-              <p>Receive funds at the speed of SMTP.</p>
-            </div>
-            <div className="IntroBar-benefits">
-              <div className="circle"/>
-              <span className="IntroBar-benefits-span">Secure</span>
-              <p>No need to worry about public and private keys.</p>
-            </div>
+                <span className="IntroBar-benefits-span">Friendly</span>
+                <p>Send to friends who know little about Eth.</p>
+              </div>
+              <div className="IntroBar-benefits">
+                <div className="circle"/>
+                  <span className="IntroBar-benefits-span">Fast</span>
+                  <p>Receive funds at the speed of SMTP.</p>
+                </div>
+              <div className="IntroBar-benefits">
+                <div className="circle"/>
+                <span className="IntroBar-benefits-span">Secure</span>
+                <p>No need to worry about public and private keys.</p>
+              </div>
           </div>
+        </div>
       </div>
-    </div>
     );
   }
 }
