cc4eedc3bb0c5c4210a1eecaf30950633142f051
Owocki <ksowocki@gmail.com>
2017-09-02 23:38:22 -0600

    smarter gas fees

--- scripts/youvegoteth/receive.js
+++ scripts/youvegoteth/receive.js
@@ -65,39 +65,42 @@ window.onload = function () {
         //find the nonce
         web3.eth.getTransactionCount(_idx,function(error,result){
             var nonce = result;
-            contract().claimTransfer.estimateGas(_idx, forwarding_address,function(error,result1){
+            web3.eth.getBalance(_idx, function(error,result){
+                var balance = result.toNumber();
+                contract().claimTransfer.estimateGas(_idx, forwarding_address,function(error,result1){
 
-                //setup raw transaction
-                if(!nonce){
-                    nonce = 0;
-                }
-                var data = contract().claimTransfer.getData(_idx, forwarding_address);
-                var payloadData = data; //??
-                var fromAccount = _idx; //???
-                var gas = 2.5 * 1000 * 100 ;
-                if(gas > maxGas){
-                    gas = maxGas;
-                }
-                var gasLimit = gas;
-                var rawTx = {
-                    nonce: web3.toHex(nonce),
-                    gasPrice: web3.toHex(2),
-                    gasLimit: web3.toHex(gasLimit),
-                    gas: web3.toHex(gas),
-                    to: contract_address,
-                    from: fromAccount,
-                    value: '0x00',
-                    data: payloadData,
-                };
-                
-                //sign & serialize raw transaction
-                var tx = new EthJS.Tx(rawTx);
-                tx.sign(new EthJS.Buffer.Buffer.from(private_key, 'hex'));
-                var serializedTx = tx.serialize();
+                    //setup raw transaction
+                    if(!nonce){
+                        nonce = 0;
+                    }
+                    var data = contract().claimTransfer.getData(_idx, forwarding_address);
+                    var payloadData = data; //??
+                    var fromAccount = _idx; //???
+                    var gas = balance;
+                    if(gas > maxGas){
+                        gas = maxGas;
+                    }
+                    var gasLimit = gas;
+                    var rawTx = {
+                        nonce: web3.toHex(nonce),
+                        gasPrice: web3.toHex(1),
+                        gasLimit: web3.toHex(gasLimit),
+                        gas: web3.toHex(gas),
+                        to: contract_address,
+                        from: fromAccount,
+                        value: '0x00',
+                        data: payloadData,
+                    };
+                    
+                    //sign & serialize raw transaction
+                    var tx = new EthJS.Tx(rawTx);
+                    tx.sign(new EthJS.Buffer.Buffer.from(private_key, 'hex'));
+                    var serializedTx = tx.serialize();
 
-                //send raw transaction
-                web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), callback);
+                    //send raw transaction
+                    web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), callback);
 
+                });
             });
         });
     };
